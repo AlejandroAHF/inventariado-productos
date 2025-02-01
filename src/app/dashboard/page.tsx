9fa0f0b62a24b3c82ae82b3039098e2d9137866d
Wkatir <110741874+wkatir@users.noreply.github.com.>
2025-01-31 22:19:56 -0600

    feat: fixing routing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,3 +7,28 @@ const page = () => {
 }
 
 export default page
+
+import LoginForm from '../components/Login'
+
+const page = () => {
+  return (
+    <>
+        <LoginForm />
+    </>
+  )
+}
+
+export default page
+
+import Register from "../components/Register"
+
+
+const page = () => {
+  return (
+    <>
+        <Register />
+    </>
+  )
+}
+
+export default page
